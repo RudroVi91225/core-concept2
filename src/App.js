a8612f92783221200b50c3711b5db499f7798aa6
RudroVi91225 <rudrodu91225@gmail.com>
2023-08-01 19:36:10 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const products = ["rakib", "jamal", "kamal", "Hasu", "khatun"]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Count></Count>
+      {
+        products.map(product => <li> Name:{product}</li>)
+      }
+      {
+        products.map(product => <Product name={product}> </Product>)
+      }
+      {/* <Product name="rakib" age="16"></Product>
+      <Product name="rakib" age="16"></Product>
+      <Product name="rakib" age="16"></Product> */}
+
+
     </div>
   );
 }
-
+function Product(props) {
+  return (
+    <div className='product'>
+      <h3>Name:{props.name}</h3>
+      <p>{props.age}</p>
+    </div>
+  )
+}
+function Count() {
+  const [count, setCount] = useState(33);
+  const increesCount = () => setCount(count + 1);
+  const decressCount = () => setCount(count - 1);
+  return (
+    <div>
+      <h3>Count :{count}</h3>
+      <button onClick={increesCount}>Increes</button>
+      <button onClick={decressCount}>Decress</button>
+    </div>
+  )
+}
 export default App;
