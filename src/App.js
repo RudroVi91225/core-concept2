ec7f8400a271821ca6d3f1f372a9c14caeeddfd4
RudroVi91225 <rudrodu91225@gmail.com>
2023-08-01 20:55:48 +0600

    tdt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const products = ["rakib", "jamal", "kamal", "Hasu", "khatun"];
@@ -28,6 +28,7 @@ function App() {
         shops.map(shop => <Shop name={shop.Name} price={shop.Price} ></Shop>)
       }
       {/* <Shop></Shop> */}
+      <Users></Users>
 
     </div>
   );
@@ -48,15 +49,46 @@ function Shop(props) {
     </div>
   )
 }
+// Show the Counter increes and decress 47/7
 function Count() {
   const [count, setCount] = useState(33);
   const increesCount = () => setCount(count + 1);
   const decressCount = () => setCount(count - 1);
   return (
     <div>
-      <h3>Count :{count}</h3>
+      <h3>Count:{count}</h3>
       <button onClick={increesCount}>Increes</button>
       <button onClick={decressCount}>Decress</button>
+    </div>
+  )
+}
+// Show the api file from Json PlaceHolder 47/8
+function Users() {
+  const [users, setusers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setusers(data))
+  }, [])
+  // console.log(users);
+  return (
+    <div>
+      <h1>Users Data</h1>
+      <p>{users.length}</p>
+      {
+        users.map(user => <ExternalUser id={user.id} name={user.name} email={user.email}></ExternalUser>)
+      }
+    </div>
+  )
+}
+function ExternalUser(props) {
+  return (
+    <div style={{
+      border: '2px solid red ', margin: '20px'
+    }}>
+      <p>ID:{props.id}</p>
+      <h3>Name: {props.name}</h3>
+      <h4>Email: {props.email}</h4>
     </div>
   )
 }
